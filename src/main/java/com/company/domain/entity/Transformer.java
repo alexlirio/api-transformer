1f24b6709a4350aa46206aa4206a8de073d5d78f
Alex Lirio <alexlirioti@gmail.com>
2021-03-29 20:21:46 -0300

    Add Transformer repository with tests
    Add:
    - Transformer repository
    - Repository crud tests
    - Entity JPA annotations
    - Spring profile test (H2 database)
    - File 'resources/data.sql' to populate the initial database.

--- src/main/java/com/company/domain/entity/Transformer.java
+++ src/main/java/com/company/domain/entity/Transformer.java
@@ -1,17 +1,78 @@
 package com.company.domain.entity;
 
-public class Transformer {
+import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.validator.constraints.Range;
+
+@Entity
+@Table(name = "transformer")
+public class Transformer implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private static final int MIN_RANGE = 1;
+	private static final int MAX_RANGE = 10;
+
+	public Transformer() {
+		super();
+	}
+
+	public Transformer(String name, TransformerTeamEnum team, Integer strength, Integer intelligence,
+			Integer speed, Integer endurance, Integer rank, Integer courage, Integer firepower, Integer skill) {
+		super();
+		this.name = name;
+		this.team = team;
+		this.strength = strength;
+		this.intelligence = intelligence;
+		this.speed = speed;
+		this.endurance = endurance;
+		this.rank = rank;
+		this.courage = courage;
+		this.firepower = firepower;
+		this.skill = skill;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	protected Long id;
+
+	@Column(name = "name", unique = true, nullable = false)
 	private String name;
+
+	@Enumerated(EnumType.STRING)
 	private TransformerTeamEnum team;
+
+	@Range(min = MIN_RANGE, max = MAX_RANGE)
 	private Integer strength;
+
+	@Range(min = MIN_RANGE, max = MAX_RANGE)
 	private Integer intelligence;
+
+	@Range(min = MIN_RANGE, max = MAX_RANGE)
 	private Integer speed;
+
+	@Range(min = MIN_RANGE, max = MAX_RANGE)
 	private Integer endurance;
+
+	@Range(min = MIN_RANGE, max = MAX_RANGE)
 	private Integer rank;
+
+	@Range(min = MIN_RANGE, max = MAX_RANGE)
 	private Integer courage;
+
+	@Range(min = MIN_RANGE, max = MAX_RANGE)
 	private Integer firepower;
+
+	@Range(min = MIN_RANGE, max = MAX_RANGE)
 	private Integer skill;
 
 	public Long getId() {
---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO transformer (id, name, team, strength, intelligence, speed, endurance, rank, courage, firepower, skill) VALUES (1, 'Optimus Prime', 'AUTOBOT', 10, 9, 8, 7, 6, 5, 4, 3);
+INSERT INTO transformer (id, name, team, strength, intelligence, speed, endurance, rank, courage, firepower, skill) VALUES (2, 'Bumblebee', 'AUTOBOT', 9, 8, 7, 6, 5, 4, 3, 2);
+INSERT INTO transformer (id, name, team, strength, intelligence, speed, endurance, rank, courage, firepower, skill) VALUES (3, 'Jazz', 'AUTOBOT', 8, 7, 6, 5, 4, 3, 2, 1);
+INSERT INTO transformer (id, name, team, strength, intelligence, speed, endurance, rank, courage, firepower, skill) VALUES (4, 'Springer', 'AUTOBOT', 7, 6, 5, 4, 3, 2, 1, 1);
+
+INSERT INTO transformer (id, name, team, strength, intelligence, speed, endurance, rank, courage, firepower, skill) VALUES (5, 'Predaking', 'DECEPTICON', 10, 9, 8, 7, 6, 5, 4, 3);
+INSERT INTO transformer (id, name, team, strength, intelligence, speed, endurance, rank, courage, firepower, skill) VALUES (6, 'Soundwave', 'DECEPTICON', 9, 8, 7, 6, 5, 4, 3, 2);
+INSERT INTO transformer (id, name, team, strength, intelligence, speed, endurance, rank, courage, firepower, skill) VALUES (7, 'Shockwave', 'DECEPTICON', 8, 7, 6, 5, 4, 3, 2, 1);
+
+INSERT INTO transformer (id, name, team, strength, intelligence, speed, endurance, rank, courage, firepower, skill) VALUES (8, 'Arcee', 'AUTOBOT', 6, 5, 4, 3, 2, 1, 1, 1);
